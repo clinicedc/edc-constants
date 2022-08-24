ab39d7934babcc63aab6b7a8b43f3f227315b963
erikvw <ew2789@gmail.com>
2022-08-24 08:57:50 -0500

    add context processor and systems check

--- edc_constants/system_checks.py
+++ edc_constants/system_checks.py
@@ -1,11 +1,11 @@
 from django.core.management import color_style
-from edc_utils.context_processor_check import edc_context_processor_check
+from edc_utils.context_processor_check import edc_context_processors_check
 
 style = color_style()
 
 
 def context_processor_check(app_configs, **kwargs):
-    return edc_context_processor_check(
+    return edc_context_processors_check(
         app_configs,
         app_label="edc_constants",
         context_processor_name="edc_constants.context_processors.constants",

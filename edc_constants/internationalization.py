afd17ee3712e1630b9daddaeb574f0900b5312db
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2023-09-13 15:02:29 +0100

    Add new language constants for internationalization
    Adds options for: Maasai, Southern Sotho, Tswana, Xhosa, and Zulu.

---
+++ edc_constants/internationalization.py
@@ -0,0 +1,35 @@
+EXTRA_LANG_INFO = {
+    # Prefer 2 digit (639-1) code over 3 digit (639-2) code
+    # Codes source: https://www.loc.gov/standards/iso639-2/php/code_list.php
+    # Local names source: https://en.wikipedia.org/wiki/List_of_ISO_639-2_codes
+    "mas": {
+        "bidi": False,
+        "code": "mas",
+        "name": "Maasai",
+        "name_local": "ɔl Maa",
+    },
+    "st": {
+        "bidi": False,
+        "code": "st",
+        "name": "Sotho, Southern",
+        "name_local": "Sesotho [southern]",
+    },
+    "tn": {
+        "bidi": False,
+        "code": "tn",
+        "name": "Tswana",
+        "name_local": "Setswana",
+    },
+    "xh": {
+        "bidi": False,
+        "code": "xh",
+        "name": "Xhosa",
+        "name_local": "isiXhosa",
+    },
+    "zu": {
+        "bidi": False,
+        "code": "zu",
+        "name": "Zulu",
+        "name_local": "isiZulu",
+    },
+}

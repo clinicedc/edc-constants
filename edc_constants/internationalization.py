2c5a852355fd552d32b03fdc7b15ca8dd59dc5d2
Jonathan Willitts <JonathanWillitts@users.noreply.github.com>
2023-09-14 11:37:43 +0100

    Add new language constants for internationalization
    Adds options for: Ganda, Runyakitara, Runyankore

--- edc_constants/internationalization.py
+++ edc_constants/internationalization.py
@@ -1,12 +1,37 @@
+"""
+EXTRA_LANG_INFO is a dictionary structure to provide meta information about
+'custom' languages not provided by Django (and already defined in:
+django.conf.locale.LANG_INFO).
+"""
+
 EXTRA_LANG_INFO = {
-    # Prefer 2 digit (639-1) code over 3 digit (639-2) code
+    # Where available, use 2 digit (639-1) code over 3 digit (639-2) code
+    #
     # Codes source: https://www.loc.gov/standards/iso639-2/php/code_list.php
     # Local names source: https://en.wikipedia.org/wiki/List_of_ISO_639-2_codes
+    "lg": {
+        "bidi": False,
+        "code": "lg",
+        "name": "Ganda",
+        "name_local": "Luganda",
+    },
     "mas": {
         "bidi": False,
         "code": "mas",
         "name": "Maasai",
         "name_local": "ɔl Maa",
+    },
+    "ry": {
+        "bidi": False,
+        "code": "ry",
+        "name": "Runyakitara",
+        "name_local": "Runyakitara",
+    },
+    "rny": {
+        "bidi": False,
+        "code": "rny",
+        "name": "Runyankore",
+        "name_local": "Runyankore",
     },
     "st": {
         "bidi": False,
